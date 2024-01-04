7a133d79fc4b547b48657d9b935a1f218ae237c1
lakshay6907 <lg1690104@gmail.com>
2024-01-04 12:49:37 +0530

    Implemented user search functionality
    - Added user search feature using Retrofit for fetching user data from the GitHub API
    - Created ViewModel to handle API requests and LiveData for observing user data
    - Set up RecyclerView to display search results in the MainActivity
    - Integrated search functionality with EditText and Button for user interaction

---
+++ app/src/main/java/com/example/speerandroidassessment/Following.kt
@@ -0,0 +1,11 @@
+package com.example.speerandroidassessment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Following : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_following)
+    }
+}
